805047bed48d85acc00bea4dc617af4858c2ac87
vivid <vivid@261house.com>
2022-03-01 22:33:43 +0900

    signup page

--- components/loginForm.js
+++ components/loginForm.js
@@ -1,46 +1,53 @@
-import React, { useCallback, useMemo, useState } from "react"
-import { Button, Form, Input } from "antd"
-import Link from "next/link"
+import React, { useCallback, useMemo, useState } from "react";
+import { Button, Form, Input } from "antd";
+import Link from "next/link";
+import useInput from "../hooks/useInput";
 // import styled from "styled-components"
 
 // const ButtonWrapper = styled.div`
 //   margin-top: 10px;
 // `
 const LoginForm = ({ setIsLoggedIn }) => {
-  const [id, setId] = useState('')
-  const [password, setPassword] = useState('')
+  const [id , onChangeId ] = useInput('')
+  const [password , onChangePassword ] = useInput('')
+  
+  
 
-  const style = useMemo(() => ({ marginTop: 10}),[])
-  const onChageId = useCallback((e) => {
-    setId(e.target.value)
-  },[])
-  const onChagePassword = useCallback((e) => {
-    setPassword(e.target.value)
-  },[])
+  const style = useMemo(() => ({ marginTop: 10 }), []);
   const onSubmitForm = useCallback(() => {
-    setIsLoggedIn(true)
-  },[id, password])
+    setIsLoggedIn(true);
+  }, [id, password]);
   return (
     <Form onFinish={onSubmitForm}>
       <div>
         <label htmlFor="user-id">아이디</label>
         <br />
-        <Input name="user-id" value={id} onChange={onChageId} required/>
+        <Input name="user-id" value={id} onChange={onChangeId} required />
       </div>
       <div>
         <label htmlFor="user-password">비밀번호</label>
         <br />
-        <Input name="user-password" value={password} type='password' onChange={onChagePassword} required/>
+        <Input
+          name="user-password"
+          value={password}
+          type="password"
+          onChange={onChangePassword}
+          required
+        />
       </div>
       <div style={style}>
-        <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
-        <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+        <Button type="primary" htmlType="submit" loading={false}>
+          로그인
+        </Button>
+        <Link href="/signup">
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
       </div>
-      <div>
-        
-      </div>
+      <div></div>
     </Form>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
